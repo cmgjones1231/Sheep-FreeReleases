e69c9fbf9aa459f3aa504f32cd2e4d1e29932ccb
cmgjones1231 <138601619+cmgjones1231@users.noreply.github.com>
2024-04-21 18:59:06 +0100

    - Fixed icedcream mlo
    Fixed missen ytyp and incorrect manifest.

---
+++ sheep_icream/fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'adamant'
+
+game 'gta5'
+lua54 'yes'
+
+author 'SheepMapping'
+
+this_is_a_map 'yes'
+
+files {
+    'timecycle_mods_vx.xml'
+}
+
+data_file 'TIMECYCLEMOD_FILE'  'timecycle_mods_vx.xml'
+
